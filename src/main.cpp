3907bdeb36fb28e6eec84f11da15feea65374607
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-02-23 15:04:24 +0100

    Try to install grpc

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/grpcpp.h>
+
+
+
+
+#include "service.grpc.pb.h"
+#include "service.pb.h"
+
+
+
 #include "components/HelloWorldComponent/hello-world-component.h"
 #include "mylib/mylib.hpp"
+
+
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using namespace profile;
+
+
+class ProfileServiceImpl final : public ProfileService::Service {
+public:
+    Status ChangeProfile(ServerContext *context, const ProfileRequest *request, ProfileReply *reply) override {
+        return Status::OK;
+    }
+};
+
+
+
 
 int main() {
     HelloWorldComponent component;
@@ -12,6 +50,20 @@ int main() {
 
     mylib::MyLib lib;
     std::cout << lib.sum(1, 3) << "\n";
+
+
+
+  std::string server_address("0.0.0.0:50051");
+
+  grpc::EnableDefaultHealthCheckService(true);
+
+//   ServerBuilder builder;
+//   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  ProfileServiceImpl service;
+//   builder.RegisterService(&service);
+//   std::unique_ptr<Server> server(builder.BuildAndStart());
+//   std::cout << "Server listening on " << server_address << std::endl;
+//   server->Wait();
 
     return 0;
 }
