148da47c252a843cfbde399497d7212d6f8b0bd3
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-02-24 12:34:08 +0100

    Update generate-proto.sh and CMake files

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,9 +1,13 @@
 
-protoc="/Users/Vladislav.Artiukhov/.local/bin/protoc"
-protoc_gen_grpc="/Users/Vladislav.Artiukhov/.local/bin/grpc_cpp_plugin"
+# Provide your paths to the protoc and protoc_gen_grpc_path components
+protoc="/Users/Vladislav.Artiukhov/.local/bin/protoc" # or: 'protoc' if it is set in PATH
+protoc_gen_grpc_path="/Users/Vladislav.Artiukhov/.local/bin/grpc_cpp_plugin" # or: `which protoc_gen_grpc_path` if protoc_gen_grpc_path is set in PATH
 
+# project path
 project_root="$(pwd)"
+# Where to store the generated sources (if changed, adjust included directories of the targets in the root CMakeLists.txt)
 dest_dir="${project_root}/generated"
+# Path to folder with proto-files
 proto_files_dir="${project_root}/proto"
 
 echo "Project root directory: ${project_root}"
@@ -18,8 +22,5 @@ echo "Generate source files from protobuf models..."
 "${protoc}" --grpc_out "${dest_dir}" \
        --cpp_out "${dest_dir}" \
        -I "${proto_files_dir}" \
-       --plugin=protoc-gen-grpc="${protoc_gen_grpc}" \
+       --plugin=protoc-gen-grpc="${protoc_gen_grpc_path}" \
        "${proto_files_dir}/"*.proto
-
-# Serves as a marker for CMake that the script has finished execution
-touch "${dest_dir}/script_execution_complete.txt"
